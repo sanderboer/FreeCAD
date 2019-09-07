9247c59b123c354a22fab76ca387c1f963d16226
Sander Boer <sanderboer@mauc.nl>
2019-09-07 08:33:55 +0200

    Too lazy to comment

--- sndr/env.sh
+++ sndr/env.sh
@@ -10,9 +10,18 @@ BUILD_DIR=${DIR}/build
 NUM_BUILD_PROCS=$(expr $(sysctl -n hw.ncpu 2> /dev/null || cat /proc/cpuinfo | grep processor | wc -l) )
 NUM_BUILD_PROCS=1
 INSTALL_DIR="/opt/freecad-${TODAY}"
-MAUCPY="/opt/miniconda3/envs/maucpy"
-PY_INC="${MAUCPY}/include/python3.7m/Python.h"
-PY_LIB="${MAUCPY}/lib/libpython3.7m.so"
+
+# MAUCPY="/opt/miniconda3/envs/maucpy"
+# PY_INC="${MAUCPY}/include/python3.7m/Python.h"
+# PY_LIB="${MAUCPY}/lib/libpython3.7m.so"
+
+
+PY_EXE="/usr/bin/python3.6"
+PPY_LIB="/usr/lib/libpython3.6m"
+PPY_LIB_PATH="/usr/lib/python3.6"
+PPY_INC="/usr/include/python3.6m/Python.h"
+
+
 
 echo "DIR:              ${DIR}"
 echo "ROOT_DIR:         ${ROOT_DIR}"
